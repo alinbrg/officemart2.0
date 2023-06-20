17c2b1f58f6a8fe94518a0117b6e188500d5c8f4
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-06-20 15:03:34 +0400

    add code to new repo

---
+++ src/js/main.js
@@ -0,0 +1,94 @@
+let catSwiper;
+function coffeeSwiperSlider() {
+	if (document.querySelector(".coffee-swiper")) {
+		const coffeeSwiper = new Swiper(".coffee-swiper", {
+			pagination: {
+				el: ".coffee-swiper .swiper-pagination",
+				type: "bullets",
+				clickable: true,
+			},
+		});
+	}
+}
+
+function catSwiperSlider() {
+	if (document.querySelector(".cat-swiper"))
+		catSwiper = new Swiper(".cat-swiper", {
+			navigation: {
+				nextEl: ".categories .swiper-button-next",
+				prevEl: ".categories .swiper-button-prev",
+			},
+			breakpoints: {
+				0: {
+					slidesPerView: 2.2,
+					spaceBetween: 8,
+				},
+				1024: {
+					slidesPerView: 4,
+					spaceBetween: 10,
+				},
+				1200: {
+					slidesPerView: 5,
+					spaceBetween: 10,
+				},
+			},
+		});
+}
+function categoryDropDown() {
+	const allCatBtn = document.querySelector(".all-cat-btn");
+	const catSwiperSld = document.querySelector(".cat-swiper");
+	const catWrapper = document.querySelector(".cat-wrapper");
+	const categories = document.querySelector(".categories");
+	if (catSwiper) {
+		allCatBtn.addEventListener("click", (e) => {
+			allCatBtn.classList.toggle("active");
+			catWrapper.classList.toggle("active");
+			categories.classList.toggle("active");
+			if (catWrapper.classList.contains("active")) {
+				catSwiper.destroy();
+				catSwiperSld.classList.add("active");
+			} else {
+				catSwiperSld.classList.remove("active");
+				catSwiperSlider();
+			}
+		});
+	}
+}
+
+const mainPageSliderBreakpoints = {
+	0: {
+		slidesPerView: 1.65,
+		spaceBetween: 10,
+	},
+	1024: {
+		slidesPerView: 4,
+		spaceBetween: 10,
+	},
+	1200: {
+		slidesPerView: 5,
+		spaceBetween: 20,
+	},
+};
+
+function mainPageProdSlider(swiper, button) {
+	if (document.querySelector(swiper)) {
+		const newProd = new Swiper(swiper, {
+			navigation: {
+				nextEl: `${button} .swiper-button-next`,
+				prevEl: `${button} .swiper-button-prev`,
+			},
+			breakpoints: mainPageSliderBreakpoints,
+		});
+	}
+}
+
+coffeeSwiperSlider();
+catSwiperSlider();
+categoryDropDown();
+
+mainPageProdSlider(".new-prod-swiper", ".new-prod-slider ");
+mainPageProdSlider(".sale-prod-swiper", ".sale-prod-slider");
+mainPageProdSlider(".wholesale-prod-swiper", ".wholesale-prod-slider");
+mainPageProdSlider(".carousel-1-prod-swiper", ".carousel-1");
+mainPageProdSlider(".carousel-2-prod-swiper", ".carousel-2");
+mainPageProdSlider(".carousel-3-prod-swiper", ".carousel-3");
